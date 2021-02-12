8ccf459a3311b5bbb5661bca916774ffb71c8b33
y4shiro <y4shironao@gmail.com>
2021-02-12 20:20:12 +0900

    17. Array 型

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 var person = {
     name: 'y4shiro',
-    age: '20'
+    age: '20',
+    hobbies: ['Sports', 'Cooking']
 };
+var favoriteActivities;
+favoriteActivities = ['Sports', 'Drive'];
+// favoriteActivities = ['Sports', 1]; // エラー
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    //  console.log(hobby.map(); // エラー
+}
--- app.ts
+++ app.ts
@@ -1,6 +1,16 @@
 const person = {
   name: 'y4shiro',
   age: '20',
+  hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports', 'Drive'];
+// favoriteActivities = ['Sports', 1]; // エラー
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+ //  console.log(hobby.map(); // エラー
+}
