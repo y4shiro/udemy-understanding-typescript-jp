5593c2fbbb25c32f4a7c9bf314c2443af2564906
y4shiro <y4shironao@gmail.com>
2021-02-12 20:11:19 +0900

    15. Object 型

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //   throw new Error('入力値が正しくありません');
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result: ';
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'y4shiro',
+    age: '20'
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,6 @@
+const person = {
+  name: 'y4shiro',
+  age: '20',
+};
+
+console.log(person.name);
