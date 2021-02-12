4bd39f36100bc7b35d9c71496ce05a787fc61b29
y4shiro <y4shironao@gmail.com>
2021-02-12 19:49:24 +0900

    13. number, string, boolean型の使い方

--- app.ts
+++ app.ts
@@ -1,11 +1,18 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   // if(typeof n1 !== 'number' || typeof n2 !== 'number') {
   //   throw new Error('入力値が正しくありません');
   // }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
 }
 
-const number1 = '5';
+const number1 = 5;
 const number2 = 2.8;
-const result = add(number1, number2);
-console.log(result);
+const printResult = true;
+const resultPhrase = 'Result: ';
+
+add(number1, number2, printResult, resultPhrase);
