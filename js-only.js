f6481d7db093ae2f9d21f6f3f98f56edf85c6517
y4shiro <y4shironao@gmail.com>
2021-02-12 10:48:35 +0900

    フォームの引数を数値に変換する

--- js-only.js
+++ js-only.js
@@ -3,7 +3,11 @@ const input1 = document.getElementById('num1');
 const input2 = document.getElementById('num2');
 
 function add(num1, num2) {
-  return num1 + num2;
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 + num2;
+  } else {
+    return +num1 + +num2;
+  }
 }
 
 button.addEventListener('click', function() {
