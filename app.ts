a21af2249de8f3ebf523c38ff537637c01e4bd88
y4shiro <y4shironao@gmail.com>
2021-02-13 17:32:30 +0900

    29. never 型

--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message, errorCode: code };
+}
+
+generateError('エラーが発生しました', 500);
