ff628a7a4b711ba05841564d5720ce40cf5dc645
y4shiro <y4shironao@gmail.com>
2021-02-13 11:41:42 +0900

    22. Literal 型

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
 }
-var combinedAges = combine(30, 30);
+var combinedAges = combine(30, 30, 'as-number');
 console.log(combinedAges);
-var combinedNames = combine('Max', 'Anna');
+var combinedStringAges = combine('30', '30', 'as-number');
+console.log(combinedStringAges);
+var combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,15 +1,23 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
   return result;
 }
 
-const combinedAges = combine(30, 30);
+const combinedAges = combine(30, 30, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine('Max', 'Anna');
+const combinedStringAges = combine('30', '30', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
 console.log(combinedNames);
