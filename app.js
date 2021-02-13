9a5ffddf21d1936166103af45a1e68750b9b9afc
y4shiro <y4shironao@gmail.com>
2021-02-13 17:03:42 +0900

    27. function 型とコールバック

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
 printResult(add(5, 12));
