bea0f6aa54150963fb93b6b07cc8b37d11b45daf
y4shiro <y4shironao@gmail.com>
2021-02-16 14:45:08 +0900

    49. アロー関数

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,30 @@
 const userName = 'Max';
 let age = 30;
 
-function add(a: number, b: number) {
-  var result;
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   var result;
+//   result = a + b;
+//   return result;
+// }
 
 if (age >= 20) {
   var isAdult = true;
 }
 // console.log(isAdult); // アクセスできてしまう
 
+// アロー関数
+const add = (a: number, b: number) => a + b;
+
+const printOutput: (output: string | number) => void = output => {
+  console.log(output);
+};
+
+printOutput(add(2, 5));
+
+const button = document.querySelector('button');
+
+if (button) {
+  button.addEventListener('click', event => {
+    console.log(event);
+  });
+}
