910abb1be10626eed722bb5c58b83199eedda0e0
y4shiro <y4shironao@gmail.com>
2021-02-16 15:05:26 +0900

    51. スプレッドオペレータ（...）

--- src/app.ts
+++ src/app.ts
@@ -29,3 +29,21 @@ if (button) {
     console.log(event);
   });
 }
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+// activeHobbies.push(hobbies[0], hobbies[1]);
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+const person = {
+  name: 'Max',
+  age: 30
+};
+
+// const copiedPerson = person; // これはオブジェクトの参照先を渡しているだけ
+const copiedPerson = {
+  ...person,
+};
+
