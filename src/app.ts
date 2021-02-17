0c184ad0d4f6b249b2484dd1f749410350c53a66
y4shiro <y4shironao@gmail.com>
2021-02-17 16:59:15 +0900

    62. プロパティ初期化のショートカット構文

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-  name: string; // フィールド
+  // id: string; // フィールド
+  // name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log('Department: ' + this.name);
+    console.log(`Department: (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
@@ -20,7 +22,7 @@ class Department {
   }
 }
 
-const accounting = new Department('Accounting');
+const accounting = new Department('d1', 'Accounting');
 accounting.addEmployee('Max');
 accounting.addEmployee('Manu');
 // accounting.employees[2] = 'Anna'; // private にアクセスしようとしてエラーとなる
