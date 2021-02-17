800ddd220998261f4d064261700ab75b2f882d3b
y4shiro <y4shironao@gmail.com>
2021-02-17 15:07:10 +0900

    61. private & public 修飾子

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string; // フィールド
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,10 +9,25 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department('Accounting');
-accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna'; // private にアクセスしようとしてエラーとなる
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
-accountingCopy.describe();
+accounting.describe();
+accounting.printEmployeeInformation();
+
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
