32c9c629d578ff10f5ec6cca07be6e1a97937de7
y4shiro <y4shironao@gmail.com>
2021-02-19 11:27:00 +0900

    69. シングルトン & private コンストラクタ

--- src/app.ts
+++ src/app.ts
@@ -36,6 +36,7 @@ class ITDepartment extends Department {
 
 class AccountingDepartment extends Department {
   private lastReport: string;
+  private static instance: AccountingDepartment;
 
   get mostRecentReport() {
     if (this.lastReport) return this.lastReport;
@@ -49,9 +50,17 @@ class AccountingDepartment extends Department {
     this.addReport(value);
   }
 
-  constructor(id: string, private reports: string[]) {
+  private constructor(id: string, private reports: string[]) {
     super(id, 'Accounting');
     this.lastReport = reports[0];
+  }
+
+  static getInstance() {
+    if (AccountingDepartment.instance) {
+      return this.instance;
+    }
+    this.instance = new AccountingDepartment('d2', []);
+    return this.instance;
   }
 
   describe() {
@@ -85,7 +94,10 @@ it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
+// const accounting = new AccountingDepartment('d2', []);
+const accounting = AccountingDepartment.getInstance();
+const accounting2 = AccountingDepartment.getInstance(); // 上の行と同じオブジェクトを参照する
+console.log(accounting, accounting2);
 
 accounting.mostRecentReport = '通期会計レポート';
 accounting.addReport('Something');
