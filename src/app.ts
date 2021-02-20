ad7a65f1ea888cc15384ac322ac2a72e728f0f42
y4shiro <y4shironao@gmail.com>
2021-02-20 11:20:29 +0900

    83. 型ガード

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,63 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric; // Number 型になる
 
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === 'string' || typeof b === 'string') {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log(emp.name);
+
+  if ('privileges' in emp) {
+    console.log('Privileges: ' + emp.privileges);
+  }
+
+  if ('startDate' in emp) {
+    console.log('Start Date: ' + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: 'Manu', startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log('運転中...');
+  }
+}
+
+class Truck {
+  drive() {
+    console.log('トラックを運転中...');
+  }
+
+  loadCargo(amount: number) {
+    console.log('荷物を載せています...' + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // if ('loadCargo' in vehicle) {
+  //   vehicle.loadCargo(1000);
+  // }
+
+  if(vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
