20de24b61d70ec30f00649a5c90aeceee578a270
y4shiro <y4shironao@gmail.com>
2021-02-22 10:49:52 +0900

    97. keyof の制約

--- src/app.ts
+++ src/app.ts
@@ -32,3 +32,9 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
 
 console.log(countAndDescribe(['Sports', 'Cooking']));
 
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
+
