843aaeef8d85eb69e0bd2cce648619b6754b3f9a
y4shiro <y4shironao@gmail.com>
2021-02-22 10:43:20 +0900

    96. もうひとつの Generic 関数

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,24 @@
 //   data.split(' ');
 // })
 
-function merge<T, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 
-const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
-  { name: 'Max', hobbies: ['Sports'] },
-  { age: 30 }
-);
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = '値がありません';
+  if (element.length > 0) {
+    descriptionText = `値は${element.length}個です`;
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Sports', 'Cooking']));
+
