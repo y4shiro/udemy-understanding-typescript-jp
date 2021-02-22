725147788231d9992656eb68dee64da66637a799
y4shiro <y4shironao@gmail.com>
2021-02-22 10:18:19 +0900

    94. Generic 関数の作成

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
-const names: Array<string> = [];
+// const names: Array<string> = [];
 // names[0].split(' ');
 
-const promise = new Promise<string>((resolve, reject) => {
-  setTimeout(() => {
-    resolve('終わりました!');
-  }, 2000);
-});
+// const promise = new Promise<string>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('終わりました!');
+//   }, 2000);
+// });
 
-promise.then(data => {
-  data.split(' ');
-})
+// promise.then(data => {
+//   data.split(' ');
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge<{ name: string; hobbies: string[] }, { age: number }>(
+  { name: 'Max', hobbies: ['Sports'] },
+  { age: 30 }
+);
+console.log(mergedObj.age);
