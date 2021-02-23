d4ca7bd5718cbf2c1d50b34c03dd6675135fe25a
y4shiro <y4shironao@gmail.com>
2021-02-23 18:29:30 +0900

    108. プロパティデコレータの詳細

--- src/app.ts
+++ src/app.ts
@@ -31,3 +31,34 @@ class Person {
 
 const pers = new Person();
 console.log(pers);
+
+// ---
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property デコレータ");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("不正な価格です - 0以下は設定できません");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
