06df3394baaa423e9f586f7e1338139f79c9d3a9
y4shiro <y4shironao@gmail.com>
2021-02-24 17:24:16 +0900

    115. デコレータによるバリデーション - 完成

--- src/app.ts
+++ src/app.ts
@@ -123,3 +123,78 @@ const p = new Printer();
 const button = document.querySelector("button")!;
 button.addEventListener('click', p.showMessage);
 
+// ---
+
+interface ValidatorConfig {
+  [prop: string]: {
+    [validatableProp: string]: string[] // ['required', 'positive']
+  }
+}
+
+const registaredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registaredValidators[target.constructor.name] = {
+    ...registaredValidators[target.constructor.name],
+    [propName]: ['required'],
+  }
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registaredValidators[target.constructor.name] = {
+    ...registaredValidators[target.constructor.name],
+    [propName]: ['positive'],
+  }
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registaredValidators[obj.constructor.name];
+  if (!objValidatorConfig) true;
+
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    console.log(prop);
+    for (const validator of objValidatorConfig[prop]){
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid &&  obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courceForm = document.querySelector("form")!;
+courceForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const titleEl = document.getElementById("title") as HTMLInputElement;
+  const priceEl = document.getElementById("price") as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!validate(createdCourse)) {
+    alert('正しく入力してください!');
+    return;
+  }
+  console.log(createdCourse);
+});
+
