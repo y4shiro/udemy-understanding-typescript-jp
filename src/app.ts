bdeac78dbf82ffd50b0d773c0553a955b41bcb28
y4shiro <y4shironao@gmail.com>
2021-02-25 17:54:32 +0900

    125. 再利用可能なバリデーション機能の作成

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,57 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === 'string'
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+  if (
+    validatableInput.min != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value >= validatableInput.min;
+  }
+  if (
+    validatableInput.max != null &&
+    typeof validatableInput.value === 'number'
+  ) {
+    isValid = isValid && validatableInput.value <= validatableInput.max;
+  }
+  return isValid;
+}
+
 // autobind decorator
-function autobind(
-  _targe: any,
-  _methodName: string,
-  descriptor: PropertyDescriptor
-) {
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   const adjDescriptor: PropertyDescriptor = {
     configurable: true,
     get() {
       const boundFn = originalMethod.bind(this);
       return boundFn;
-    }
-  }
+    },
+  };
   return adjDescriptor;
 }
 
@@ -25,35 +65,62 @@ class ProjectInput {
   mandayInputElement: HTMLInputElement;
 
   constructor() {
-    this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+    this.templateElement = document.getElementById(
+      'project-input',
+    )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
-    const importedNode = document.importNode(this.templateElement.content, true);
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true,
+    );
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input'; 
+    this.element.id = 'user-input';
 
-    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.mandayInputElement = this.element.querySelector('#manday') as HTMLInputElement;
+    this.titleInputElement = this.element.querySelector(
+      '#title',
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description',
+    ) as HTMLInputElement;
+    this.mandayInputElement = this.element.querySelector(
+      '#manday',
+    ) as HTMLInputElement;
 
     this.configure();
     this.attach();
   }
 
   private gatherUserInput(): [string, string, number] | void {
-    const enterdTitle = this.titleInputElement.value;
-    const enterdDescription = this.descriptionInputElement.value;
-    const enterdManday = this.mandayInputElement.value;
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredManday = this.mandayInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    };
+    const mandayValidatable: Validatable = {
+      value: +enteredManday,
+      required: true,
+      min: 1,
+      max: 1000,
+    };
     if (
-      enterdTitle.trim().length === 0 ||
-      enterdDescription.trim().length === 0 ||
-      enterdManday.trim().length === 0
-     ) {
-       alert('入力値が正しくありません。');
-       return;
-     } else {
-       return[enterdTitle, enterdDescription, +enterdManday];
-     }
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(mandayValidatable)
+    ) {
+      alert('入力値が正しくありません。再度お試しください。');
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredManday];
+    }
   }
 
   private clearInputs() {
@@ -83,4 +150,3 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
-
