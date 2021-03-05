0f848d792ae1af4bd80e68dbbcae13b078105dd0
y4shiro <y4shironao@gmail.com>
2021-03-05 19:49:45 +0900

    145. ESモジュールの利用

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,49 +1,47 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { Draggable } from '../models/drag-drop.js';
+import { Project } from '../models/project.js';
+import { Component } from './base-component.js';
+import { autobind } from '../decorators/autobind.js';
 
-namespace App {
-  // ProjectItem Class
-  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable{
-    private project: Project;
+// ProjectItem Class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable{
+  private project: Project;
 
-    get manday() {
-      if (this.project.manday < 20) {
-        return this.project.manday.toString() + '人日';
-      } else {
-        return (this.project.manday / 20) + '人月';
-      }
+  get manday() {
+    if (this.project.manday < 20) {
+      return this.project.manday.toString() + '人日';
+    } else {
+      return (this.project.manday / 20) + '人月';
     }
+  }
 
-    constructor(hostId: string, project: Project) {
-      super('single-project', hostId, false, project.id);
-      this.project = project;
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
 
-      this.configure();
-      this.renderContent();
-    }
+    this.configure();
+    this.renderContent();
+  }
 
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData('text/plain', this.project.id);
-      event.dataTransfer!.effectAllowed = 'move';
-    }
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData('text/plain', this.project.id);
+    event.dataTransfer!.effectAllowed = 'move';
+  }
 
-    dragEndHandler(_: DragEvent) {
-      console.log('Drag 終了');
-    }
+  dragEndHandler(_: DragEvent) {
+    console.log('Drag 終了');
+  }
 
-    configure() {
-      this.element.addEventListener('dragstart', this.dragStartHandler)
-      this.element.addEventListener('dragend', this.dragEndHandler)
-    }
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler)
+    this.element.addEventListener('dragend', this.dragEndHandler)
+  }
 
-    renderContent() {
-      this.element.querySelector('h2')!.textContent = this.project.title;
-      this.element.querySelector('h3',)!.textContent = this.manday;
-      this.element.querySelector('p')!.textContent = this.project.description;
-    }
+  renderContent() {
+    this.element.querySelector('h2')!.textContent = this.project.title;
+    this.element.querySelector('h3',)!.textContent = this.manday;
+    this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,13 +1,11 @@
-namespace App {
-  // Drag & Drop
-  export interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-    dragEndHandler(event: DragEvent): void;
-  }
+// Drag & Drop
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHandler(event: DragEvent): void;
+}
 
-  export interface DragTarget {
-    dragOverHandler(event: DragEvent): void;
-    dropHandler(event: DragEvent): void;
-    dragLeaveHandler(event: DragEvent): void;
-  }
+export interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
 }
