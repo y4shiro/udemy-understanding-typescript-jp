f1e724525bec4ad01165d8708ab63895ef4c3ef8
y4shiro <y4shironao@gmail.com>
2021-03-07 20:44:49 +0900

    182. State の利用 & 型

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'TS コースの完了' }];
+  const [todos, setTodos] = useState<Todo[]>([]);
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random.toString(), text: text }]);
   };
   return (
     <div className="App">
