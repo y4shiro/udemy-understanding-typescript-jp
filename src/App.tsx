3726779d5621bf6e00bf17cb3db07b88d7d22bbd
y4shiro <y4shironao@gmail.com>
2021-03-07 19:39:25 +0900

    179. Props の利用 & Props の型

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import TodoList from './components/TodoList';
+
 const App: React.FC = () => {
+  const todos = [{ id: 't1', text: 'TS コースの完了' }];
   return (
-    <div className="App"></div>
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
   );
 }
 
