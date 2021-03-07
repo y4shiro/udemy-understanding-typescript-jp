d32a563498d1c69dada3bfd7cb433b3f9dec1e1b
y4shiro <y4shironao@gmail.com>
2021-03-07 20:29:35 +0900

    181. コンポーネント間の連携

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,12 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{ id: 't1', text: 'TS コースの完了' }];
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
